9eed0b35a3056b9515e09f0d0369c64e26b44d1a
Cesars117 <gilloo156@gmail.com>
2026-04-27 20:30:18 -0500

    Fix: Add getItemById and clean up next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: '20mb',
     },
   },
 };
+
 
 
 export default nextConfig;
