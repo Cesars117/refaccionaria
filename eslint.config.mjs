358992ddfabf4f89c7ca84786a0d8926c816e8e0
Cesars117 <gilloo156@gmail.com>
2026-05-09 01:49:21 -0500

    fix: ignore server.js in eslint to pass github actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,6 +21,7 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "server.js",
     // Custom ignores for data scripts:
     "add-data.*",
     "seed-simple.*",
